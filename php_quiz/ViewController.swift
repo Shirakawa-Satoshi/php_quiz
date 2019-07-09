52fa8bb319a7bc70f27f91a3c9657ef3ae2d566a
白川聡 <kyabetsu3104@gmail.com>
2019-07-09 16:08:32 +0900

    Initial Commit

---
+++ php_quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  php_quiz
+//
+//  Created by 白川聡 on 2019/07/09.
+//  Copyright © 2019 白川聡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ php_quizTests/php_quizTests.swift
@@ -0,0 +1,34 @@
+//
+//  php_quizTests.swift
+//  php_quizTests
+//
+//  Created by 白川聡 on 2019/07/09.
+//  Copyright © 2019 白川聡. All rights reserved.
+//
+
+import XCTest
+@testable import php_quiz
+
+class php_quizTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
